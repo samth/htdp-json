7cedb6083f1078f921af7c7d0fa777bb4eb2b2d5
Sam Tobin-Hochstadt <samth@cs.indiana.edu>
2017-11-01 09:21:48 -0400

    convert

--- json.rkt
+++ json.rkt
@@ -1,20 +1,28 @@
 #lang racket/base
 
-(require 2htdp/batch-io)
-(require net/url)
+(require net/url racket/match json racket/file)
+(provide read-json/web read-json/file
+         (struct-out object))
 
-(require json)
-
-(provide read-json/web read-json/file)
+(define-struct object (members) #:transparent)
 
 ; read-json/web : String -> JSExpr
 ; Retrieves the remote file at the given URL and returns JSON data
 (define (read-json/web url)
   (let-values ([(status header content)
                 (http-sendrecv/url (string->url url))])
-    (read-json content)))
+    (convert (read-json content))))
 
 ; read-json/file : String -> JSExpr
 ; Retrieves the local file with the given name and returns JSON data
 (define (read-json/file file)
-  (string->jsexpr (read-file file)))
+  (convert (string->jsexpr (file->string file))))
+
+;; convert to a more student-friendly format
+(define (convert jsexpr)
+  (match jsexpr
+    [(or 'null #t #f (? number?) (? string?)) jsexpr]
+    [(? list? l) (map convert jsexpr)]
+    [(? hash? h)
+     (make-object (hash-map h (lambda (k v) (list (symbol->string k)
+                                                  (convert v)))))]))
